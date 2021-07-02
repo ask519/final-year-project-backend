2a6048e93e0b720e55b5b9cf14455dddc67cb3cf
Abdul Khan <40662043+abdulskhan47@users.noreply.github.com>
2021-07-02 22:24:42 +0530

    updated test file for different conditions

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,24 +5,50 @@ sys.path.append(os.path.join(os.path.dirname(__file__),'../','app_main'))
 
 from app_main.app import app, main
 import unittest
-
+import pytest
 
 class FlaskTestCase(unittest.TestCase):
     def test_trial(self):
 
         tester = app.test_client(self)
-        response = tester.get('/testpage',content_type='application/json')
+        response = tester.get('/testpage',content_type='application//json')
         print(response.json["description"])
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json["description"], 'My First Heading')
     
+    
+
     def test_dataflow(self):
-        
-        tester = app.test_client(self)
-        response = tester.post('/testpage', data= dict( caption_image = open(os.path.join(os.getcwd(),'app_main','caption_images','doggy269.jpg'),'rb')) )
-        print("Hello")
-        print(response.json)
-        expected_strings = ["dog","beach"]
-        self.assertTrue(all(x in str(response.json["description"]) for x in expected_strings ))
+        #this works fine, now we have to work on error handling
+        input_value = [
+            [['app_main', 'caption_images', 'doggy269.jpg'], ['dog', 'beach']],
+            [['app_main', 'caption_images', 'playing.jpeg'], ['playing', 'soccer']],
+            [['app_main', 'caption_images', 'laptop.png'], ['man', 'bench']],
+            ]#test conditions for different image formats
+        #expected=[['dog','beach']]
+        for i,j in input_value:
+            tester = app.test_client(self)
+            response = tester.post('/testpage', data= dict( caption_image = open(os.path.join(os.getcwd(),i[0],i[1],i[2]),'rb')) )
+            print(response.json)
+            expected_strings = j
+            self.assertTrue(all(x in str(response.json["description"]) for x in expected_strings ))
+        #below code is for checking for error when file format is incorrect
+        input_value = [
+            [['app_main', 'caption_images','shh.txt'], ["Non-Existent or Invalid File Upload"]]
+        ]
+        for i,j in input_value:
+            tester = app.test_client(self)
+            response = tester.post('/testpage', data= dict( caption_image = open(os.path.join(os.getcwd(),i[0],i[1],i[2]),'rb')) )
+            print(response.json)
+            expected_strings = j
+            pytest.raises(TypeError)
+        #test if file is non-existant
+        input_value = [
+            [['app_main', 'caption_images',''], ["Non-Existent or Invalid File Upload"]]
+        ]
+        for i,j in input_value:
+            tester = app.test_client(self)
+            pytest.raises(FileNotFoundError)
+
 
     
